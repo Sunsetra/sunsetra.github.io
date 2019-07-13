ef16cd54c9640c05cea0095b41024e8fa5711f8b
B-R-D <xingxuanma@gmail.com>
2019-07-13 18:03:32 +0900

    Ver.1.09-20190713 转移至github.io平台

---
+++ js/gensei.js
@@ -0,0 +1,59 @@
+// 首先声明所需的jQuery变量
+const text = $('.all-text'); // 整个内容版面
+const eye = $('#eye'); // 鼠标点击红眼
+const navi = $('li'); // 导航栏点击变红
+
+// 鼠标点击时红眼特效
+const eyeEffect = () => {
+  text.on('click', (e) => {
+    let centerOffset;
+    if (window.outerWidth <= 1023) {
+      centerOffset = 30;
+    } else {
+      centerOffset = 37;
+    }
+    eye.css('left', `${e.pageX - centerOffset}px`);
+    eye.css('top', `${e.pageY - centerOffset}px`);
+    eye.show();
+    setTimeout(() => { eye.fadeOut(250); }, 100);
+  });
+};
+
+// 左侧导航栏点击进入
+const clickFadeIn = () => {
+  let naviNum;
+  let naviId;
+  $('li').on('click', function () {
+    // 取每个导航元素的ID进行比对并赋值以找到对应文本的ID
+    naviNum = $(this).attr('id').substring(1, 3);
+    if (Number.isNaN(Number(naviNum))) {
+      if (Number.isNaN(Number(naviNum[0]))) {
+        naviId = '#shinkai';
+      } else {
+        naviId = `#gensei${naviNum[0]}`;
+      }
+    } else {
+      naviId = `#gensei${naviNum}`;
+    }
+    $('#info').hide();
+    $('.zukan').hide();
+    $(naviId).fadeIn(350);
+  });
+};
+
+// 这里是开启网页时隐藏zukan显示info，并设置点击渐入的传入参数
+const main = () => {
+  $('.zukan').hide();
+  $('#info').show();
+  clickFadeIn();
+  eyeEffect();
+
+  // 使所有导航栏元素点击后变成红色，鼠标松/离开该按钮时变成原色
+  navi.on({
+    mouseover() { $(this).css('cursor', 'pointer'); },
+    mousedown() { $(this).css('color', 'red'); },
+    'mouseout mouseup': function () { $(this).css('color', '#CCCCCC'); },
+  });
+};
+
+$(document).ready(main);
